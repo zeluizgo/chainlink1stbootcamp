718fe65defcf5b138524364213fd4c87b9a7a412
Jose Luiz Clemente Goncalves <joseluizcg@hotmail.com>
2024-04-27 16:35:59 -0300

    Create Register.sol
    File creation

---
+++ Register.sol
@@ -0,0 +1,18 @@
+// Inicio
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+ 
+contract Register {
+    string private info;
+
+    function getInfo() public view returns (string memory) {
+        return info;
+    }
+
+    function setInfo(string memory _info) public {
+        info = _info;
+    }
+}
+
+// Fim
